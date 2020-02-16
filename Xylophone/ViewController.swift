4653a596ef9b751d82448fc7c6ce05f308d9e5b6
muzammil22 <muzzammilmuneer@gmail.com>
2020-02-16 23:42:23 +0500

    playing different sounds on keypress and changing attributes

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,32 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+//        print(sender.currentTitle)
+        sender.alpha = 0.5
+        print("start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+            print("end")
+            // your code here
+        }
+        playSound(title: sender.currentTitle!)
+    }
     
-
+    func playSound(title: String) {
+        print(title)
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
